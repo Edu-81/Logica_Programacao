582202c1e225a4bb4351ab19aeaab58b5a2a3748
Edu-81 <164214330+Edu-81@users.noreply.github.com>
2024-07-02 19:27:29 -0300

    Add files via upload

---
+++ algoritmo_variaveis.py
@@ -0,0 +1,5 @@
+print ('Vamos criar um retângulo')
+base = float(input('Digite a base: '))
+altura = float(input('Digite a altura: '))
+area = base * altura
+print ('A área total do triangulo é de',area)
---
+++ condicionais.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número: '))
+if numero > 0:
+    print (f'O número digitado é POSITIVO')
+elif numero == 0:
+    print (f'O número digitado é ZERO')
+else:
+    print (f'O número digitado é NEGATIVO')
